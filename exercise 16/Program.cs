7b05ab7e587540db495da87b632603ebdd79ab4f
Mason Oltermann <m.oltermann@gmail.com>
2021-06-22 16:50:53 -0500

    Add project files.

---
+++ exercise 16/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace exercise_16
+{
+    class Program
+    {
+
+        static void Divide(double x, double y)
+        {
+            try
+            {
+                if (y == 0)
+                {
+                    throw new ArgumentOutOfRangeException("can't divide by 0");
+                }
+                Console.WriteLine(x / y);
+            }
+            catch(ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Not a valid input");
+            }
+            }
+
+
+        static void CheckFileExtension (string fileName)
+        {
+            try
+            {
+                string endOfFileName = fileName.Substring(fileName.Length - 3, 3);
+                if (endOfFileName == ".cs")
+                {
+                    Console.WriteLine("Give a point");
+                }
+                else
+                {
+                    Console.WriteLine("No point awarded");
+                }
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("invalid.");
+            }
+            
+            }
+        
+         
+ 
+        static void Main(string[] args)
+        {
+            Divide(7, 0);
+            CheckFileExtension("");
+        
+        
+        
+        
+        }
+    }
+}
